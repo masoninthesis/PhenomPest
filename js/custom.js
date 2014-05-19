0d562ec31aac5b60590dd9b93876da4b89ee2093
Mason Lawlor <mason.lawlor@me.com>
2014-05-18 22:58:17 -0500

    colors and icons in top tab

--- js/custom.js
+++ js/custom.js
@@ -479,3 +479,17 @@ $(function() {
 $('#name').focus(function() {
      $('#success').html('');
   });
+  
+  
+/* tabs */
+
+$(document).ready(function() {
+    $("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
+        e.preventDefault();
+        $(this).siblings('a.active').removeClass("active");
+        $(this).addClass("active");
+        var index = $(this).index();
+        $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+        $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+    });
+});
